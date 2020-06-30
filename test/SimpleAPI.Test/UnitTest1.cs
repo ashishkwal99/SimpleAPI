629840a7647f7b7f91df9ed412886f519dedc660
Ashish Khandelwal <ashishkwal99@gmail.com>
2020-06-30 10:33:09 -0700

    Initial commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+using System.Collections.Generic;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+        {
+        WeatherForecastController controller = new WeatherForecastController(null);
+
+        [Fact]
+        public void GetReturnsByName()
+        {
+            var returnValue = controller.Get();
+            IEnumerator<WeatherForecast> enumerator =  returnValue.GetEnumerator();
+            enumerator.MoveNext();
+
+            Assert.Equal(40, enumerator.Current.TemperatureC);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
